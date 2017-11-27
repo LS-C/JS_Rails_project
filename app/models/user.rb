5a2201213507e4f6896dcbb2afe6ed3c0f667635
LS-C <suzy.cho111@gmail.com>
2017-11-27 16:37:54 -0500

    added full_name class method to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,10 @@ class User < ApplicationRecord
     has_many :items, foreign_key: 'seller_id'
     has_many :orders, through: :items
     has_many :purchases, foreign_key: 'buyer_id', :class_name => "Order"
+
+    def full_name
+        self.first_name + " " + self.last_name
+    end
+
 end
 
