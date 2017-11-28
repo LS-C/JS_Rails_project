b7b865a4ce2c0690030cf86be468903dcb09d73f
LS-C <suzy.cho111@gmail.com>
2017-11-27 23:17:17 -0500

    added partial form to create items to users/show view

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,6 +14,7 @@ class ItemsController < ApplicationController
 
     def create
         @item = Item.create(item_params)
+        render json: @item
     end
 
     def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   resources :orders
-  resources :users
   resources :items
+  resources :users do
+    resources :items
+  end
+
+
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
